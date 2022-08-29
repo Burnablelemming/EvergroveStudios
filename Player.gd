4aac70cf770aa5149e44aec729f6e7168b0af3a7
Kooper Young <youngkooper@gmail.com>
2022-08-28 22:20:37 -0700

    The players rotation is now tweened, badly, but it works

--- Player.gd
+++ Player.gd
@@ -4,23 +4,22 @@ var rotationTargets : Array
 var currentTarget : int = 0
 var targetsCount : int
 var initalCheck : int = 1
+var tween
 
 var HUD : CanvasLayer
 
 func _ready():
-	pass
-
-	#Get the correct node by using a clever get children method
-	for child in get_parent().get_children():
-		if child is CanvasLayer:
-			HUD = get_parent().get_child(child.get_index())
-			break
+	tween = get_node("Tween")
+	
+	rotation += 0.1
+	print(rotation)
 
 #When using KinematicBody2D you should use _physics_process
 #It's called the same amount of times per second, always. 
 #This makes physics and motion calculation work in a more predictable way
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("ui_right"):
+		print("Button hit but does move")
 		if targetsCount == 1:
 			currentTarget = 0
 		elif targetsCount == 2:
@@ -33,15 +32,15 @@ func _physics_process(_delta):
 				currentTarget = 0
 			else:
 				currentTarget += 1
-		#print("Current target: ", currentTarget, rotationTargets[currentTarget].get_global_position())
-		look_at(rotationTargets[currentTarget].get_global_position())
-	
-	if Input.is_action_just_pressed("ui_accept"):
-		var tween = get_node("Tween")
-		tween.interpolate_property(self, "position", position, rotationTargets[currentTarget].get_global_position(), 3, Tween.TRANS_QUINT, Tween.EASE_OUT)
+		print("Current target: ", currentTarget, rotationTargets[currentTarget].get_global_position())
+#		look_at(rotationTargets[currentTarget].get_global_position())
+
+		tween.interpolate_property(self, "rotation", rotation, position.angle_to_point(rotationTargets[currentTarget].get_global_position()), 0.5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 		tween.start()
-		#print(abs(position.x) + abs(rotationTargets[currentTarget].get_global_position().x) + abs(position.y) + abs(rotationTargets[currentTarget].get_global_position().y))
-		#HUD.update_movement_points(abs(position.x) + abs(rotationTargets[currentTarget].get_global_position().x) + abs(position.y) + abs(rotationTargets[currentTarget].get_global_position().y))
+#
+	if Input.is_action_just_pressed("ui_accept"):
+		tween.interpolate_property(self, "position", position, rotationTargets[currentTarget].get_global_position(), 2, Tween.TRANS_QUINT, Tween.EASE_OUT)
+		tween.start()
 
 func _on_Area2D_area_entered(area):
 	rotationTargets.clear()
@@ -56,5 +55,5 @@ func _on_Area2D_area_entered(area):
 	currentTarget = 0
 	targetsCount = rotationTargets.size()
 	#print(rotationTargets)
-	look_at(rotationTargets[currentTarget].get_global_position())
+	#look_at(rotationTargets[currentTarget].get_global_position())
 	initalCheck = 0
