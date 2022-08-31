87a86ce7471bae5a4c31e1a302b91e3b7b1f85cc
Kooper Young <youngkooper@gmail.com>
2022-08-30 21:59:26 -0700

    Much better looking environment created

---
+++ Assets/Tilemaps/tileset_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="tileset_3" tilewidth="16" tileheight="16" tilecount="36" columns="12">
+ <image source="original_pngs/set_3.png" width="192" height="48"/>
+</tileset>
