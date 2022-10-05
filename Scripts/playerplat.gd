91cb3ff13c038e375140cfe305b14a8b5d430fe0
Kooper Young <youngkooper@gmail.com>
2022-10-04 20:42:20 -0700

    touching up movement, it is not done yet

--- Scripts/playerplat.gd
+++ Scripts/playerplat.gd
@@ -1,23 +1,17 @@
 extends KinematicBody2D
 
-#Export Variables
-#1, 10, 30 are way to fucking slow
-# is a bit slow
-export var GRAVITY = 50
-
 const ZERO = 0
 
-#1, 10 are way to fucking slow
-#100 is a bit fast
-export var speed = 5
-export var friction = 15
+#Export Variables
+export var gravity = 50
+export var speed = 10
+export var friction = 10
 export var maxAcceleration = 75
-export var jumpForce = 50
-
-var acceleration = 0
+export var jumpForce = -500
 
 #describes the last direction the player was traveling in
 var lastDirection : String
+var acceleration = Vector2(0,0)
 
 #Debug Variables###############################################################
 var currentPlayerMovement : Vector2
@@ -27,22 +21,38 @@ func _ready():
 
 #UPGRADE - Works, but feels REALLY floaty
 func _physics_process(_delta):
+	#Left and right movement
 	if Input.is_action_pressed("ui_right"):
-		if acceleration < maxAcceleration:
-			acceleration += speed
+		acceleration.x += speed
 	elif Input.is_action_pressed("ui_left"):
-		if acceleration > (-maxAcceleration):
-			acceleration -= speed
-	elif Input.is_action_just_pressed("ui_up"):
-		move_and_slide(Vector2(0,jumpForce))
-		print("jumping")
-		
+		acceleration.x -= speed
 	else:
-		if acceleration < 0:
-			acceleration += friction
-		elif acceleration > 0:
-			acceleration -= friction
-	currentPlayerMovement = move_and_slide(Vector2(acceleration,GRAVITY))
+		applyFriction()
+	if Input.is_action_just_pressed("ui_up") and acceleration.y == 0:
+		acceleration.y = jumpForce
 	
+	
+	acceleration.y += gravity
+
+	#TODO - Explain why these are different
+	acceleration = move_and_slide(acceleration)
+	#move_and_slide(acceleration)
 	print(acceleration)
+	
+func applyFriction():
+	#print("was called")
+	#If player is moving
+	if acceleration.x != 0:
+		#print("accel not zero")
+		#Player was moving right
+		if acceleration.x > 0:
+			#print("moving right")
+			acceleration.x -= friction
+		#Player was moving left
+		elif acceleration.x < 0:
+			#print("moving right")
+			acceleration.x += friction
+	#accel was zero
+	else:
+		return
 
