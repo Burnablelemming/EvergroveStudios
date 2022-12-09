5b266bdc803121f10652a7804fcab19054526c06
Kooper Young <youngkooper@gmail.com>
2022-12-09 13:26:58 -0800

    NEW JUMP!
    The character has a new jump which you can hold for an extra umph. Not perfect, kinda broken, will fix

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,12 +5,17 @@ const ZERO = 0
 onready var spr = $spr
 onready var anim = $anim
 
-#Export Variables
-export var gravity = 30
-export var speed = 20
-export var friction = 20
-export var maxAcceleration = 180
-export var jumpForce = -400
+
+var gravity = 30
+var speed = 20
+var friction = 20
+var maxAcceleration = 180
+
+#Jump-specific vars
+var jumpHold = -1
+var jumpBurst = -400
+var jumpMax = -10
+var jumpCur = 0
 
 #describes the last direction the player was traveling in
 var lastDirection : String
@@ -22,14 +27,12 @@ var currentPlayerMovement : Vector2
 func _ready():
 	pass
 
-#UPGRADE - Works, but feels REALLY floaty
 func _physics_process(_delta):
 	#Left and right movement
 	if Input.is_action_pressed("ui_right"):
 		if(acceleration.x + speed) <= maxAcceleration:
 			acceleration.x += speed
 		anim.play("walkRight")
-
 	elif Input.is_action_pressed("ui_left"):
 		if(acceleration.x - speed) >= -maxAcceleration:
 			acceleration.x -= speed
@@ -37,10 +40,25 @@ func _physics_process(_delta):
 	else:
 		anim.play("idleRight")
 		applyFriction()
-	if Input.is_action_just_pressed("ui_up") and acceleration.y == 0:
-		acceleration.y = jumpForce
 	
-	acceleration.y += gravity
+	#FIXME - You can kinda float on your way up
+	#if acceleration.y is a positive number you are going down
+	if Input.is_action_just_pressed("ui_up") and acceleration.y == 0:
+		acceleration.y = jumpBurst
+	if Input.is_action_pressed("ui_up"):
+		if(acceleration.y > 0):
+			acceleration.y += gravity
+		elif(jumpCur >= jumpMax):
+			acceleration.y += jumpHold
+			jumpCur += jumpHold
+			print("Cur Jump = ", jumpCur, "maxJump = ", jumpMax)
+		else:
+			acceleration.y += gravity
+		print(acceleration)
+	else:
+		jumpCur = 0
+		acceleration.y += gravity
+
 
 	#TODO - Explain why these are different
 	acceleration = move_and_slide(acceleration)
